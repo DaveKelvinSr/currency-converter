fbb55fbeac7e68b66302acf9782183b2607fdeb5
David Kelvin <davidkelvinsr@gmail.com>
2018-06-30 20:59:42 +0300

    second commit

--- js/converter.js
+++ js/converter.js
@@ -1,26 +1,15 @@
 function convertScreen(mainID) {
-	var screen = mainID;
-	var initialized = false;
+	const screen = mainID;
+	let initialized = false;
 	
 	return {
 		
-		if ('serviceWorker' in navigator) {
- 			 window.addEventListener('load', function() {
-    				navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      					// Registration was successful
-      					console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    				}, function(err) {
-     					 // registration failed :(
-      					console.log('ServiceWorker registration failed: ', err);
-    				});
-  			});
-		}
 		init: function() {
 			if (initialized) {
 				return;
 			}
 		
-			var currency1 = document.getElementById('currency1')
+			const currency1 = document.getElementById('currency1')
 			currency1.oninvalid = function(e) {
 				e.target.setCustomValidity("");
 				if (e.target.validity.valid == false) {
@@ -33,7 +22,7 @@ function convertScreen(mainID) {
 				}
 			};
 			
-			var rate = document.getElementById('rate')
+			const rate = document.getElementById('rate')
 			rate.oninvalid = function(e) {
 				e.target.setCustomValidity("");
 				if (e.target.validity.valid == false) {
@@ -49,16 +38,16 @@ function convertScreen(mainID) {
 				function(evt) {
 					evt.preventDefault();
 					if ($(evt.target).parents('form')[0].checkValidity()) {
-						var currency = this.serializeForm();
-						var html = "<span>" + currency.currency1*currency.rate + "</span>";
+						const currency = this.serializeForm();
+						const html = "<span>" + currency.currency1*currency.rate + "</span>";
 						$('#currency2 span').remove();
 						$('#currency2').append(html);
 					}
 				}.bind(this)
 
 			);
-			// left if here
-			var request = indexedDB.open('convertDB');
+			/* left if here
+			const request = indexedDB.open('convertDB');
 			request.onsuccess = function(event) {
 				database = request.result;
 				this.configureData();
@@ -68,14 +57,14 @@ function convertScreen(mainID) {
 				database = event.target.result;
 				var objectStoreConvert = database.createObjectStore("convert",
 					{keyPath: "id", autoIncrement: true });
-			}; //just here
+			};*/ //just here
 
 			initialized = true;
 		},
 		
 		serializeForm: function() {
-			var inputFields = $(screen).find('form :input');
-			var result = {};
+			const inputFields = $(screen).find('form :input');
+			let result = {};
 			$.each(inputFields, function(index, value) {
 				if ($(value).attr('name')) {
 					result[$(value).attr('name')] = $(value).val();
